f32de3e4231e725e76763ec09d0efab30fe71a24
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-06 12:57:54 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('button');
+// grabbing the id another way
+// const input = document.querySelector('#button');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
